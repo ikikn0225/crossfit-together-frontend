6ff3d79893f2f6cdeacbde2f191a22c664725f03
wsx2792 <wsx2792@gmail.com>
2021-08-24 23:30:20 +0900

    chore: header border line정리

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,13 @@ import { LOCALSTORAGE_TOKEN } from "@/constants";
 import { clearCookie } from "@/cookie";
 import { useMe } from "@/hooks/useMe"
 import { useHistory } from "react-router-dom";
-import { _HeaderNotVerifyContainer ,_HeaderContainer ,_HeaderLogo ,_HeaderLogOutButton, _HeaderUl, _HeaderLl } from "../theme/components/_Header";
+import { _HeaderNotVerifyContainer ,_HeaderContainer } from "../theme/components/_Header";
 import styled, { css } from 'styled-components';
 import Burger from "./menu";
 
 const Nav = styled.nav`
     width: 100%;
     height: 70px;
-    border-bottom: 2px solid #f1f1f1;
     padding: 0 20px;
     display: flex;
     justify-content: space-between;
@@ -24,13 +23,6 @@ const Nav = styled.nav`
 export const Header:React.FC = () => {
     const { client, data } = useMe();
     const history = useHistory();
-    const logOutClick = () => {
-        client.cache.reset().then(() => {
-            clearCookie('authorization');
-            location.reload();
-        })
-        history.push("/");
-    }
     
     return (
         <>
@@ -43,7 +35,7 @@ export const Header:React.FC = () => {
             }
             <Nav>
                 <div className="logo">
-                    <img src="../../public/images/logo_black_fake.jpg" />
+                    <img src="../../public/images/logo_black.jpg" />
                 </div>
                 <Burger />
             </Nav>
