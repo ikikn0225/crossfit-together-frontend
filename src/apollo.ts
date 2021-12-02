f4dce4c3296ca9703098baecce41d7cf94a4cb5a
HeadCastle <wsx2792@gmail.com>
2021-12-02 13:28:34 +0900

    chore: heroku 수정

--- src/apollo.ts
+++ src/apollo.ts
@@ -14,7 +14,9 @@ export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri: process.env.NODE_ENV === "production"
+    ? 'https://crossfitogether0225.herokuapp.com/graphql'
+    : 'http://localhost:4000/graphql',  
 });
 
 const authLink = setContext((_, { headers }) => {
